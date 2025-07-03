c01c7b884044522b67c5c1a5aa0dd5ad4410564e
dohuyminh <minhdohuy2111@gmail.com>
2025-07-03 17:32:00 +1000

    displayed init board

---
+++ build/chess_board/CMakeFiles/chess_board.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chess_board.dir/chess_board.cpp.o"
+  "CMakeFiles/chess_board.dir/chess_board.cpp.o.d"
+  "libchess_board.a"
+  "libchess_board.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/chess_board.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
